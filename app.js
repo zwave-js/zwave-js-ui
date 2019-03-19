f88f60773025ceb9d5891fa3a7c35906c44d9387
Daniel Lando <daniel.sorridi@gmail.com>
2019-03-19 10:06:18 +0100

    Fixed duplicated messages when restarting client #17

--- app.js
+++ app.js
@@ -150,7 +150,8 @@ app.post('/api/settings', function(req, res) {
   jsonStore.put(store.settings, req.body)
   .then(data => {
     res.json({success: true, message: "Configuration updated successfully"});
-    gw.close(() => startGateway());
+    gw.close();
+    startGateway();
   }).catch(err => {
     debug(err);
     res.json({success: false, message: err.message})
@@ -183,7 +184,8 @@ process.removeAllListeners('SIGINT');
 
 process.on('SIGINT', function() {
   debug('Closing...');
-  gw.close(() => process.exit());
+  gw.close();
+  process.exit();
 });
 
 module.exports = app;
