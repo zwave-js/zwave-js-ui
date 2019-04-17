e8029dc38c6cbab98b6ea60259217584db6586e9
Daniel Lando <daniel.sorridi@gmail.com>
2019-04-17 10:00:09 +0200

    Better log of 404 errors

--- app.js
+++ app.js
@@ -169,7 +169,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  debug(err);
+  console.log("%s %s %d - Error: %s", req.method, req.url, err.status, err.message);
 
   // render the error page
   res.status(err.status || 500);
