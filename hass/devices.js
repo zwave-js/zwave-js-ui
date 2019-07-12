c75d0881f562b541ce26abed8a662323ba9b23c8
Daniel Lando <daniel.sorridi@gmail.com>
2019-07-12 17:52:45 +0200

    [fix] Change setpoint when mode changes

--- hass/devices.js
+++ hass/devices.js
@@ -5,6 +5,8 @@ module.exports = {
     object_id: 'thermostat',
     values: ['64-1-0', '49-1-1', '67-1-1', '67-1-2'],
     mode_map: {'off': 'Off', 'heat': 'Heat (Default)', 'cool': 'Cool'},
+    setpoint_topic: { "Heat (Default)": '67-1-1', "Cool": '67-1-2' },
+    default_setpoint: '67-1-1',
     discovery_payload: {
       min_temp: 15,
       max_temp: 30,
@@ -13,7 +15,6 @@ module.exports = {
       mode_command_topic: true,
       current_temperature_topic: '49-1-1',
       current_temperature_template: '{{ value_json.value }}',
-      temperature_state_topic: { "Heat (Default)": '67-1-1', "Cool": '67-1-2' },
       temperature_state_template: '{{ value_json.value }}',
       temperature_command_topic: true
     }
