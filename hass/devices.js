26112c59c937eaf66828f7de92b723a155aa02ab
Daniel Lando <daniel.sorridi@gmail.com>
2019-07-17 11:15:39 +0200

    [feat] GE 1724 fan dimmer device

--- hass/devices.js
+++ hass/devices.js
@@ -1,22 +1,50 @@
 /* eslint-disable quotes */
-module.exports = {
-  '411-1-1': [{ // Heatit Thermostat TF 021 (ThermoFloor AS)
-    type: 'climate',
-    object_id: 'thermostat',
-    values: ['64-1-0', '49-1-1', '67-1-1', '67-1-2'],
-    mode_map: {'off': 'Off', 'heat': 'Heat (Default)', 'cool': 'Cool'},
-    setpoint_topic: { "Heat (Default)": '67-1-1', "Cool": '67-1-2' },
-    default_setpoint: '67-1-1',
+
+// Place here repeated patterns
+var GE_DIMMER = [
+  { // GE 1724 Dimmer
+    type: 'fan',
+    object_id: 'dimmer',
+    values: ['38-1-0'],
     discovery_payload: {
-      min_temp: 15,
-      max_temp: 30,
-      modes: ['off', 'heat', 'cool'],
-      mode_state_topic: '64-1-0',
-      mode_command_topic: true,
-      current_temperature_topic: '49-1-1',
-      current_temperature_template: '{{ value_json.value }}',
-      temperature_state_template: '{{ value_json.value }}',
-      temperature_command_topic: true
+      command_topic: "38-1-0",
+      speed_command_topic: "38-1-0",
+      speed_state_topic: "38-1-0",
+      state_topic: "38-1-0",
+      speeds: ["off", "low", "medium", "high"],
+      payload_low_speed: 24,
+      payload_medium_speed: 50,
+      payload_high_speed: 99,
+      payload_off: 0,
+      payload_on: 99,
+      state_value_template: "{% if (value_json.value | int) == 0 %} 0 {% else %} 99 {% endif %}",
+      speed_value_template: "{% if (value_json.value | int) == 25 %}  24  {% elif (value_json.value | int) == 51 %} 50 {% elif (value_json.value | int) == 99 %} 99 {% else %}  0  {% endif %}"
     }
-  }]
+  }
+]
+
+module.exports = {
+  '411-1-1': [
+    { // Heatit Thermostat TF 021 (ThermoFloor AS)
+      type: 'climate',
+      object_id: 'thermostat',
+      values: ['64-1-0', '49-1-1', '67-1-1', '67-1-2'],
+      mode_map: { 'off': 'Off', 'heat': 'Heat (Default)', 'cool': 'Cool' },
+      setpoint_topic: { "Heat (Default)": '67-1-1', "Cool": '67-1-2' },
+      default_setpoint: '67-1-1',
+      discovery_payload: {
+        min_temp: 15,
+        max_temp: 30,
+        modes: ['off', 'heat', 'cool'],
+        mode_state_topic: '64-1-0',
+        mode_command_topic: true,
+        current_temperature_topic: '49-1-1',
+        current_temperature_template: '{{ value_json.value }}',
+        temperature_state_template: '{{ value_json.value }}',
+        temperature_command_topic: true
+      }
+    }
+  ],
+  '99-12340-18756': GE_DIMMER,
+  '99-12593-18756': GE_DIMMER
 }
