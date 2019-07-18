6986a0aea10b8ea3e5f374e687d6a33193523dae
Daniel Lando <daniel.sorridi@gmail.com>
2019-07-18 11:49:31 +0200

    [fix] Edit payload templates based on gateway payload type

--- hass/devices.js
+++ hass/devices.js
@@ -1,27 +1,25 @@
 /* eslint-disable quotes */
 
 // Place here repeated patterns
-var GE_DIMMER = [
-  { // GE 1724 Dimmer
-    type: 'fan',
-    object_id: 'dimmer',
-    values: ['38-1-0'],
-    discovery_payload: {
-      command_topic: "38-1-0",
-      speed_command_topic: "38-1-0",
-      speed_state_topic: "38-1-0",
-      state_topic: "38-1-0",
-      speeds: ["off", "low", "medium", "high"],
-      payload_low_speed: 24,
-      payload_medium_speed: 50,
-      payload_high_speed: 99,
-      payload_off: 0,
-      payload_on: 99,
-      state_value_template: "{% if (value_json.value | int) == 0 %} 0 {% else %} 99 {% endif %}",
-      speed_value_template: "{% if (value_json.value | int) == 25 %}  24  {% elif (value_json.value | int) == 51 %} 50 {% elif (value_json.value | int) == 99 %} 99 {% else %}  0  {% endif %}"
-    }
+var FAN_DIMMER = {
+  type: 'fan',
+  object_id: 'dimmer',
+  values: ['38-1-0'],
+  discovery_payload: {
+    command_topic: "38-1-0",
+    speed_command_topic: "38-1-0",
+    speed_state_topic: "38-1-0",
+    state_topic: "38-1-0",
+    speeds: ["off", "low", "medium", "high"],
+    payload_low_speed: 24,
+    payload_medium_speed: 50,
+    payload_high_speed: 99,
+    payload_off: 0,
+    payload_on: 99,
+    state_value_template: "{% if (value_json.value | int) == 0 %} 0 {% else %} 99 {% endif %}",
+    speed_value_template: "{% if (value_json.value | int) == 25 %}  24  {% elif (value_json.value | int) == 51 %} 50 {% elif (value_json.value | int) == 99 %} 99 {% else %}  0  {% endif %}"
   }
-]
+}
 
 module.exports = {
   '411-1-1': [
@@ -45,6 +43,6 @@ module.exports = {
       }
     }
   ],
-  '99-12340-18756': GE_DIMMER,
-  '99-12593-18756': GE_DIMMER
+  '99-12340-18756': [FAN_DIMMER], // GE 1724 Dimmer
+  '99-12593-18756': [FAN_DIMMER] // GE 1724 Dimmer
 }
