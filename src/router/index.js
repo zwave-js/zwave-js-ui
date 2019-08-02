d0046eefa23ea0250d3b540e61632f312c5c82d7
Daniel Lando <daniel.sorridi@gmail.com>
2019-08-02 11:07:39 +0200

    [fix] [fix] Fixed app title

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'ControlPanel',
+      name: 'Control Panel',
       component: ControlPanel,
       props: true
     },
