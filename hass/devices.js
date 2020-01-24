8f933edfaee40214ac9a0a04c00bf2dc7321f4aa
Daniel Lando <daniel.sorridi@gmail.com>
2020-01-24 10:18:40 +0100

    feat: Hass device Heatit TF 056 (Thermofloor)

--- hass/devices.js
+++ hass/devices.js
@@ -22,7 +22,7 @@ var FAN_DIMMER = {
 }
 
 // Eurotronic Spirit Z-Wave Plus Thermostat
-var SPIRIT_ZWAVE_PLUS =     {
+var SPIRIT_ZWAVE_PLUS = {
   type: 'climate',
   object_id: 'thermostat',
   values: ['64-1-0', '49-1-1', '67-1-1', '67-1-11'],
@@ -66,6 +66,27 @@ module.exports = {
       values: ['64-1-0', '49-1-1', '67-1-1', '67-1-2'],
       mode_map: { 'off': 'Off', 'heat': 'Heat (Default)', 'cool': 'Cool' },
       setpoint_topic: { "Heat (Default)": '67-1-1', "Cool": '67-1-2' },
+      default_setpoint: '67-1-1',
+      discovery_payload: {
+        min_temp: 15,
+        max_temp: 30,
+        modes: ['off', 'heat', 'cool'],
+        mode_state_topic: '64-1-0',
+        mode_command_topic: true,
+        current_temperature_topic: '49-1-1',
+        current_temperature_template: '{{ value_json.value }}',
+        temperature_state_template: '{{ value_json.value }}',
+        temperature_command_topic: true
+      }
+    }
+  ],
+  '411-514-3': [ // Heatit Thermostat TF 056 (ThermoFloor AS)
+    {
+      type: 'climate',
+      object_id: 'thermostat',
+      values: ['64-1-0', '49-1-1', '67-1-1', '67-1-2'],
+      mode_map: { 'off': 'Off', 'heat': 'Heat', 'cool': 'Cool' },
+      setpoint_topic: { "Heat": '67-1-1', "Cool": '67-1-2' },
       default_setpoint: '67-1-1',
       discovery_payload: {
         min_temp: 15,
