3f11ae208848f0788b90235e5e0449107d214f1a
Raymond Julin <raymond.julin@gmail.com>
2020-01-29 11:23:01 +0100

    fix: Disable host checks in webpack to allow connecting externally in dev

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -31,6 +31,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     hot: true,
     contentBase: false, // since we use CopyWebpackPlugin.
     compress: true,
+    disableHostCheck: true,
     host: HOST || config.dev.host,
     port: PORT || config.dev.port,
     open: config.dev.autoOpenBrowser,
