ec80288ff35cf5c6bbbeeda77d868fc62ab5ec73
Chris Nesbitt-Smith <chris@cns.me.uk>
2020-04-28 16:04:12 +0100

    chore: test coverage lib/debug (#390)

---
+++ test/lib/debug.test.js
@@ -0,0 +1,33 @@
+const chai = require('chai')
+const rewire = require('rewire')
+chai.should()
+
+describe('#debug', () => {
+  let mod = rewire('../../lib/debug')
+  let fun = mod.__get__('init')
+
+  it('returns debug extend', () =>
+    mod('foo').namespace.should.equal('z2m:foo')
+  )
+
+  describe('set process.env.DEBUG', () => {
+    before(() => {
+      mod.__get__('log').disable()
+      process.env.DEBUG = 'ff'
+      fun()
+    })
+    it('should disable logging', () =>
+      mod.__get__('log').enabled('z2m:aa').should.be.false
+    )
+  })
+  describe('unset process.env.DEBUG', () => {
+    before(() => {
+      mod.__get__('log').disable()
+      delete process.env.DEBUG
+      fun()
+    })
+    it('should enable logging', () => {
+      return mod.__get__('log').enabled('z2m:aa').should.be.true
+    })
+  })
+})
