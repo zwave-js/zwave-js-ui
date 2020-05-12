e90aeb62d67a134e9e937917fa58bde1a32342d4
Daniel Lando <daniel.sorridi@gmail.com>
2020-05-12 11:13:18 +0200

    fix: allow empty node name/location #463 (#468)

--- src/components/ControlPanel.vue
+++ src/components/ControlPanel.vue
@@ -535,14 +535,14 @@ export default {
       val || this.closeDialog()
     },
     newName (val) {
-      var match = val ? val.match(/[a-zA-Z0-9_-]+/g) : []
+      var match = val ? val.match(/[a-zA-Z0-9_-]+/g) : [val]
 
       this.nameError = match[0] !== val
         ? 'Only a-zA-Z0-9_- chars are allowed'
         : null
     },
     newLoc (val) {
-      var match = val ? val.match(/[a-zA-Z0-9_-]+/g) : []
+      var match = val ? val.match(/[a-zA-Z0-9_-]+/g) : [val]
 
       this.locError = match[0] !== val
         ? 'Only a-zA-Z0-9_- chars are allowed'
