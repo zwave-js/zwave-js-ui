84dc8572510c31622fb10cc140c667d1e6aaf227
Daniel Lando <daniel.sorridi@gmail.com>
2020-05-29 09:47:07 +0200

    fix: offline icons not showing #508 (#537)

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,10 +3,14 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
-import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
 
 Vue.use(Vuetify)
 
-const opts = {}
+const opts = {
+  icons: {
+    iconfont: 'md'
+  }
+}
 
 export default new Vuetify(opts)
