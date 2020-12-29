91998e04aab334b99d5a7f8026dec30de9492533
Andreas Hochsteger <andreas.hochsteger@gmail.com>
2020-12-29 11:39:15 +0100

    feat(ui): add nodes table filter + persistent UI settings (#90)
    Nodes table filter functionality:
    * Is a (refactored) port of the nodes table filter from zwave2mqtt
    * Generic support for header filtering (supports boolean, date, number and string columns)
    * Supports Regex matching for string columns
    * Adds form input validation
    
    Persisted settings in browser local storage:
    * Dark mode
    * Show hidden nodes
    * Number of items per page on nodes table
    * Column filters
    * Column sorting

---
+++ src/components/nodes-table/ColumnFilterNumber.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-card-text>
+    <v-form v-model="valid">
+      <v-row>
+        <v-col>
+          <v-text-field
+            type="number"
+            label="Min"
+            v-model="value.min"
+            :rules="rules.min"
+            clearable
+            @change="change"
+          ></v-text-field>
+        </v-col>
+        <v-col>
+          <v-text-field
+            type="number"
+            label="Max"
+            v-model="value.max"
+            :rules="rules.max"
+            clearable
+            @change="change"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-select
+            v-model="value.values"
+            :items="items"
+            label="Values"
+            clearable
+            chips
+            deletableChips
+            dense
+            multiple
+            @change="change"
+          ></v-select>
+        </v-col>
+      </v-row>
+    </v-form>
+  </v-card-text>
+</template>
+
+<script>
+import ColumnFilterHelper from '@/modules/ColumnFilterHelper'
+export default {
+  props: {
+    value: {
+      type: Object,
+      default: () => ColumnFilterHelper.defaultFilter('string'),
+      required: true
+    },
+    items: {
+      type: Array,
+      default: () => [],
+      required: true
+    }
+  },
+  data () {
+    return {
+      valid: false,
+      rules: {
+        min: [
+          v => !v || v >= 0 || 'Minimum should not be negative',
+          v =>
+            !v ||
+            !this.value.max ||
+            v <= this.value.max ||
+            'Minimum should not be above maximum'
+        ],
+        max: [
+          v => !v || v >= 0 || 'Maximum should not be negative',
+          v =>
+            !v ||
+            !this.value.min ||
+            v >= this.value.min ||
+            'Maximum should not be below minimum'
+        ]
+      }
+    }
+  },
+  methods: {
+    change () {
+      this.$emit('change', this.value, this.valid)
+    }
+  }
+}
+</script>
