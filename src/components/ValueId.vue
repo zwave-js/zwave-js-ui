31f324df5ad6c28240cc15555953f13a8e8f6eae
Daniel Lando <daniel.sorridi@gmail.com>
2021-01-14 16:29:58 +0100

    feat(ui): rgb colors support
    Fixes #78

--- src/components/ValueId.vue
+++ src/components/ValueId.vue
@@ -29,6 +29,42 @@
       @click:append-outer="updateValue(value)"
     ></v-text-field>
 
+    <v-text-field
+      mask="!#XXXXXX"
+      style="max-width: 250px;margin: auto;"
+      flat
+      solo
+      v-if="value.type == 'color'"
+      v-model="color"
+      :label="'[' + value.id + '] ' + value.label"
+      :append-outer-icon="!disable_send ? 'send' : null"
+      :hint="value.description || ''"
+      @click:append-outer="updateValue(value)"
+    >
+      <template v-slot:append>
+        <v-menu
+          v-model="menu"
+          top
+          nudge-bottom="105"
+          nudge-left="16"
+          :close-on-content-click="false"
+        >
+          <template v-slot:activator="{ on }">
+            <div :style="pickerStyle" v-on="on" />
+          </template>
+          <v-card>
+            <v-card-text class="pa-0">
+              <v-color-picker
+                hide-mode-switch
+                v-model="color"
+                flat
+              />
+            </v-card-text>
+          </v-card>
+        </v-menu>
+      </template>
+    </v-text-field>
+
     <v-select
       v-if="value.list"
       :items="value.states"
@@ -105,7 +141,34 @@ export default {
       type: Boolean
     }
   },
-  computed: {},
+  data () {
+    return {
+      menu: false
+    }
+  },
+  computed: {
+    color: {
+      // getter
+      get: function () {
+        return '#' + (this.value.newValue || 'ffffff').toUpperCase()
+      },
+      // setter
+      set: function (v) {
+        this.value.newValue = v ? v.substr(1, 7) : null
+      }
+    },
+    pickerStyle () {
+      if (this.value.type !== 'color') return null
+      return {
+        backgroundColor: this.color,
+        cursor: 'pointer',
+        height: '30px',
+        width: '30px',
+        borderRadius: this.menu ? '50%' : '4px',
+        transition: 'border-radius 200ms ease-in-out'
+      }
+    }
+  },
   methods: {
     updateValue (v, customValue) {
       this.$emit('updateValue', v, customValue)
