a2b3aa2ddc2ea66169af86db4ff02795fe13a11c
Daniel Lando <daniel.sorridi@gmail.com>
2021-01-20 16:15:14 +0100

    chore: fix package-lock.json (#284)
    * chore: fix package-lock.json
    
    * fix: removed friendly errors not compatible with webpack 5

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -8,7 +8,6 @@ const path = require('path')
 const baseWebpackConfig = require('./webpack.base.conf')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
 
 const HOST = process.env.HOST
@@ -41,7 +40,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
       : false,
     publicPath: config.dev.assetsPublicPath,
     proxy: config.dev.proxyTable,
-    quiet: true, // necessary for FriendlyErrorsPlugin
+    quiet: false,
     watchOptions: {
       poll: config.dev.poll
     }
@@ -89,20 +88,6 @@ module.exports = new Promise((resolve, reject) => {
       process.env.PORT = port
       // add port to devServer config
       devWebpackConfig.devServer.port = port
-
-      // Add FriendlyErrorsPlugin
-      devWebpackConfig.plugins.push(
-        new FriendlyErrorsPlugin({
-          compilationSuccessInfo: {
-            messages: [
-              `Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`
-            ]
-          },
-          onErrors: config.dev.notifyOnErrors
-            ? utils.createNotifierCallback()
-            : undefined
-        })
-      )
 
       resolve(devWebpackConfig)
     }
