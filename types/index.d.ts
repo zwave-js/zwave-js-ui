450b20789ff8d5cda94e5df1aeaac67e6bdcf698
Daniel Lando <daniel.sorridi@gmail.com>
2021-01-27 14:09:06 +0100

    refactor: added some types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,15 @@
+import { ZwavejsServer } from '@zwave-js/server'
 import { EventEmitter } from 'events'
-import { type } from 'os'
-import { CommandClass, InterviewStage, NodeStatus, ValueType, ZWaveOptions } from 'zwave-js'
+import { MqttClient as Client } from 'mqtt'
+import { Socket } from 'net'
+import {
+  CommandClass,
+  InterviewStage,
+  NodeStatus,
+  ValueType,
+  ZWaveNode,
+  ZWaveOptions
+} from 'zwave-js'
 
 export type Z2MValueIdState = {
   text: string
@@ -101,88 +110,180 @@ export type Z2MNode = {
 }
 
 export enum GatewayType {
-    VALUEID,
-    NAMED,
-    MANUAL
+  VALUEID,
+  NAMED,
+  MANUAL
 }
 
 export enum PayloadType {
-    JSON_TIME_VALUE,
-    VALUEID,
-    RAW
+  JSON_TIME_VALUE,
+  VALUEID,
+  RAW
 }
 
 export type GatewayValue = {
-    device: string,
-    value: Z2MValueId,
-    topic: string,
-    device_class: string,
-    icon: string,
-    postOperation: string,
-    enablePoll: boolean,
-    pollInterval: number,
-    parseSend: boolean,
-    sendFunction: string,
-    parseReceive: boolean,
-    receiveFunction: string
+  device: string
+  value: Z2MValueId
+  topic: string
+  device_class: string
+  icon: string
+  postOperation: string
+  enablePoll: boolean
+  pollInterval: number
+  parseSend: boolean
+  sendFunction: string
+  parseReceive: boolean
+  receiveFunction: string
 }
 
 export type LogLevel = 'silly' | 'verbose' | 'debug' | 'info' | 'warn' | 'error'
 
 export type GatewayConfig = {
-    type: GatewayType,
-    payloadType: PayloadType,
-    nodeNames: boolean,
-    ignoreLoc: boolean,
-    sendEvents: boolean,
-    ignoreStatus: boolean,
-    includeNodeInfo: boolean,
-    publishNodeDetails: boolean,
-    retainedDiscovery: boolean,
-    entityTemplate: string,
-    hassDiscovery: boolean,
-    discoveryPrefix: string,
-    logEnabled: boolean,
-    logLevel: LogLevel,
-    logToFile: boolean,
-    values: GatewayValue[]
+  type: GatewayType
+  payloadType: PayloadType
+  nodeNames: boolean
+  ignoreLoc: boolean
+  sendEvents: boolean
+  ignoreStatus: boolean
+  includeNodeInfo: boolean
+  publishNodeDetails: boolean
+  retainedDiscovery: boolean
+  entityTemplate: string
+  hassDiscovery: boolean
+  discoveryPrefix: string
+  logEnabled: boolean
+  logLevel: LogLevel
+  logToFile: boolean
+  values: GatewayValue[]
 }
 
 export type MqttConfig = {
-    name: string,
-    host: string,
-    port: number,
-    reconnectPeriod: number,
-    prefix: string,
-    qos: 0 | 1 | 2,
-    retain: boolean,
-    clean: boolean,
-    store: boolean,
-    allowSelfSigned: boolean,
-    key: string,
-    cert: string,
-    ca: string,
-    auth: boolean,
-    username: string,
-    password: string
+  name: string
+  host: string
+  port: number
+  reconnectPeriod: number
+  prefix: string
+  qos: 0 | 1 | 2
+  retain: boolean
+  clean: boolean
+  store: boolean
+  allowSelfSigned: boolean
+  key: string
+  cert: string
+  ca: string
+  auth: boolean
+  username: string
+  password: string
 }
 
 export type ZwaveConfig = {
-    port: string,
-    networkKey: string,
-    serverEnabled: boolean,
-    serverPort: number,
-    logEnabled: boolean,
-    logLevel: LogLevel,
-    commandsTimeout: number,
-    plugin: string,
-    options: ZWaveOptions
+  port: string
+  networkKey: string
+  serverEnabled: boolean
+  serverPort: number
+  logEnabled: boolean
+  logLevel: LogLevel
+  commandsTimeout: number
+  plugin: string
+  options: ZWaveOptions
 }
 
 export interface MqttClient extends EventEmitter {
-    config: MqttConfig
-    on (event: 'writeRequest', listener: (parts: string[], payload: any) => void): this
-    on (event: 'broadcastRequest', listener: (parts: string[], payload: any) => void): this
-    on (event: 'apiCall', listener: (topic: string, apiNema: string, payload: any) => void): this
-    on (event: 'connect', listener: () => void): this
+  config: MqttConfig
+  toSubscribe: string[]
+  clientID: string
+  client: Client
+  error?: string
+  closed: boolean
+  connected: boolean
+  broadcastPrefix: string
+  eventsPrefix: string
+
+  on(
+    event: 'writeRequest',
+    listener: (parts: string[], payload: any) => void
+  ): this
+  on(
+    event: 'broadcastRequest',
+    listener: (parts: string[], payload: any) => void
+  ): this
+  on(
+    event: 'apiCall',
+    listener: (topic: string, apiNema: string, payload: any) => void
+  ): this
+  on(event: 'connect', listener: () => void): this
+  on(event: 'brokerStatus', listener: (online: boolean) => void): this
+  on(event: 'hassStatus', listener: (online: boolean) => void): this
+}
+
+export type Z2MDriverInfo = {
+  homeId: string
+  name: string
+  controllerId: string
+}
+
+export enum ZwaveClientStatus {
+  CONNECTED = 'connected',
+  DRIVER_READY = 'driver ready',
+  SCAN_DONE = 'scan done',
+  DRIVER_FAILED = 'driver failed',
+  CLOSED = 'closed'
+}
+
+export enum EventSource {
+  DRIVER = 'driver',
+  CONTROLLER = 'controller',
+  NODE = 'node'
+}
+
+export interface ZwaveClient extends EventEmitter {
+  cfg: ZwaveConfig
+  soclet: Socket
+  closed: boolean
+  driverReady: boolean
+  scenes: any[]
+  nodes: Z2MNode[]
+  storeNodes: Z2MNode[]
+  devices: Map<string, Z2MNode>
+  driverInfo: Z2MDriverInfo
+  status: ZwaveClientStatus
+  error: boolean | string
+  scanComplete: boolean
+  cntStatus: string
+
+  server: ZwavejsServer
+  statelessTimeouts: Map<string, NodeJS.Timeout>
+  commandsTimeout: NodeJS.Timeout
+  reconnectTimeout: NodeJS.Timeout
+  healTimeout: NodeJS.Timeout
+
+  on(event: 'nodeStatus', listener: (node: Z2MNode) => void): this
+  on(
+    event: 'event',
+    listener: (source: EventSource, eventName: string, ...args: any) => void
+  ): this
+  on(event: 'scanComplete', listener: () => void): this
+  on(
+    event: 'notification',
+    listener: (
+      node: Z2MNode,
+      label: string,
+      parameters: string | number | Buffer
+    ) => void
+  ): this
+  on(event: 'nodeRemoved', listener: (node: Z2MNode) => void): this
+  on(
+    event: 'valueChanged',
+    listener: (valueId: Z2MValueId, node: Z2MNode) => void
+  ): this
+}
+
+export interface Z2MGateway {
+  config: GatewayConfig
+  mqtt: MqttClient
+  zwave: ZwaveClient
+
+  topicValues: Map<string, string>
+  discovered: Map<string, HassDevice>
+  topicLevels: number[]
 }
