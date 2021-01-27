22e6e41de5577c4b835402d1731ed58890dc932d
Daniel Lando <daniel.sorridi@gmail.com>
2021-01-27 15:23:03 +0100

    fix: types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,12 +1,16 @@
 import { ZwavejsServer } from '@zwave-js/server'
 import { EventEmitter } from 'events'
-import { MqttClient as Client } from 'mqtt'
+import { MqttClient as Client, IClientPublishOptions } from 'mqtt'
 import { Socket } from 'net'
+import { type } from 'os'
 import {
+  Association,
   CommandClass,
   InterviewStage,
   NodeStatus,
+  ValueID,
   ValueType,
+  ZWaveNode,
   ZWaveOptions
 } from 'zwave-js'
 
@@ -41,6 +45,16 @@ export type Z2MValueId = {
   states?: ZValueIdState[]
   list: boolean
   lastUpdate?: number
+}
+
+export type Z2MValueIdScene = Z2MValueId & {
+  timeout: number
+}
+
+export type Z2MScene = {
+  sceneId: number
+  label: string
+  values: Z2MValueIdScene[]
 }
 
 export type Z2MDeviceClass = {
@@ -213,6 +227,21 @@ export interface MqttClient extends EventEmitter {
   on(event: 'connect', listener: () => void): this
   on(event: 'brokerStatus', listener: (online: boolean) => void): this
   on(event: 'hassStatus', listener: (online: boolean) => void): this
+
+  getClientTopic(suffix: string): string
+  close(): Promise<void>
+  getStatus(): any
+  updateClientStatus(connected: boolean): void
+  publishVersion(): void
+  update(config: MqttConfig): void
+  subscribe(topic: string): void
+  publish(
+    topic: string,
+    data: any,
+    options: IClientPublishOptions,
+    prefix: string
+  ): void
+  getTopic(topic: any, set: any): string
 }
 
 export type Z2MDriverInfo = {
@@ -240,7 +269,7 @@ export interface ZwaveClient extends EventEmitter {
   soclet: Socket
   closed: boolean
   driverReady: boolean
-  scenes: any[]
+  scenes: Z2MScene[]
   nodes: Z2MNode[]
   storeNodes: Z2MNode[]
   devices: Map<string, Z2MNode>
@@ -275,6 +304,83 @@ export interface ZwaveClient extends EventEmitter {
     event: 'valueChanged',
     listener: (valueId: Z2MValueId, node: Z2MNode) => void
   ): this
+
+  scheduleHeal(): void
+  getNode(nodeId: number): ZWaveNode
+  getZwaveValue(idString: any): ValueID
+  heal(): void
+  updateDevice(
+    hassDevice: HassDevice,
+    nodeId: number,
+    deleteDevice: boolean
+  ): void
+  addDevice(hassDevice: HassDevice, nodeId: number): void
+  storeDevices(
+    devices: Map<string, HassDevice>,
+    nodeId: number,
+    remove: boolean
+  ): Promise<void>
+  close(): Promise<void>
+  getStatus(): { driverReady: boolean; status: boolean; config: GatewayConfig }
+  addEmptyNodes(): void
+  getGroups(nodeId: number, ignoreUpdate: boolean): Promise<void>
+  getAssociations(nodeId: number, groupId: number): Promise<Association[]>
+  addAssociations(
+    nodeId: number,
+    groupId: number,
+    associations: Association[]
+  ): Promise<void>
+  removeAssociations(
+    nodeId: number,
+    groupId: number,
+    associations: Association[]
+  ): Promise<void>
+  removeAllAssociations(nodeId: number): Promise<void>
+  removeNodeFromAllAssociations(nodeId: number): Promise<void>
+  refreshNeighbors(): void
+  connect(): Promise<void>
+  sendToSocket(evtName: string, data: any): void
+  _setNodeName(nodeid: number, name: string): Promise<boolean>
+  _setNodeLocation(nodeid: number, loc: string): Promise<boolean>
+  _createScene(label: string): Promise<boolean>
+  _removeScene(sceneid: number): Promise<boolean>
+  _setScenes(scenes: Z2MScene[]): Promise<Z2MScene[]>
+  _getScenes(): Z2MScene[]
+  _sceneGetValues(sceneid: number): Z2MValueIdScene[]
+  _addSceneValue(
+    sceneid: number,
+    valueId: Z2MValueIdScene,
+    value: any,
+    timeout: number
+  ): Promise<void>
+  _removeSceneValue(sceneid: number, valueId: Z2MValueIdScene): Promise<void>
+  _activateScene(sceneId: number): boolean
+  getNodes(): Z2MNode[]
+  getInfo(): Map<string, any>
+  refreshValues(nodeId: number): Promise<void>
+  replaceFailedNode(nodeId: number, secure: any): Promise<boolean>
+  startInclusion(secure: boolean): Promise<boolean>
+  startExclusion(): Promise<boolean>
+  stopExclusion(): Promise<boolean>
+  stopInclusion(): Promise<boolean>
+  healNode(nodeId: number): Promise<boolean>
+  isFailedNode(nodeId: number): Promise<boolean>
+  removeFailedNode(nodeId: number): Promise<void>
+  refreshInfo(nodeId: number): Promise<void>
+  beginFirmwareUpdate(
+    nodeId: number,
+    fileName: string,
+    data: Buffer
+  ): Promise<void>
+  abortFirmwareUpdate(nodeId: number): Promise<void>
+  beginHealingNetwork(): Promise<boolean>
+  stopHealingNetwork(): Primise<boolean>
+  hardReset(): Promise<void>
+  callApi(
+    apiName: string,
+    ...args: any
+  ): Promise<{ success: boolean; message: string; result: any; args: any[] }>
+  writeValue(valueId: Z2MValueId, value: number | string): Promise<void>
 }
 
 export interface Z2MGateway {
@@ -285,4 +391,35 @@ export interface Z2MGateway {
   topicValues: Map<string, string>
   discovered: Map<string, HassDevice>
   topicLevels: number[]
+
+  start(): void
+  parsePayload(
+    payload: any,
+    valueId: Z2MValueId,
+    valueConf: GatewayValue
+  ): /* error */ any
+  close(): void
+  nodeTopic(node: Z2MNode): string
+  valueTopic(
+    node: Z2MNode,
+    valueId: Z2MValueId,
+    returnObject: boolean
+  ): { valueConf: GatewayValue; topic: string } | string
+  rediscoverNode(nodeID: number): void
+  disableDiscovery(nodeID: number): void
+  publishDiscovery(
+    hassDevice: HassDevice,
+    nodeId: number,
+    deleteDevice: boolean,
+    update: boolean
+  ): void
+  setDiscovery(
+    nodeId: number,
+    hassDevice: HassDevice,
+    deleteDevice: boolean
+  ): void
+  rediscoverAll(): void
+  discoverDevice(node: Z2MNode, hassDevice: HassDevice): void
+  discoverClimates(node: Z2MNode): void
+  discoverValue(node: Z2MNode, vId: string): void
 }
