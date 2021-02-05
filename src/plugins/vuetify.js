898c2a75e93b161119eb2fbb9391cfdb4f7999cf
Andreas Hochsteger <andreas.hochsteger@oeamtc.at>
2021-02-05 10:39:48 +0100

    refactor: revert inclusion of mdi icons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,7 +4,6 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
-import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 
 Vue.use(Vuetify)
 
