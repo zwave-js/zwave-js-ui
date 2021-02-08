656e56d544bbf9b896b10ce1c38c6b8e1f28b164
Daniel Lando <daniel.sorridi@gmail.com>
2021-02-08 10:00:38 +0100

    feat(ui): improved mesh graph (#458)
    * feat(ui): improve mesh graph
    
    Fixes #384
    
    * fix: something shows up
    
    * fix: lint errors
    
    * fix: filter failed nodes
    
    * fix: nodeList undefined caused by d3 migration v5->v6
    
    * fix: testing node list
    
    * refactor: format test node list files
    
    * fix: hide speed dial
    
    * fix: hammer events
    
    * feat: legend
    
    * feat: layout edges grouping
    
    * fix: lint issues
    
    * fix: improvments
    
    * fix: improved performances
    
    * fix: height
    
    * fix: nodes location
    
    * fix: battery devices
    
    * fix: add node is frequent listineing
    
    * fix: hops colors
    
    * fix: node name
    
    * fix: lint issues
    
    * fix: better check node forwards prop
    
    * fix: loader
    
    * fix: debounce refresh
    
    * refactor: add collapsible config panel
    
    Co-authored-by: Andreas Hochsteger <andreas.hochsteger@oeamtc.at>

--- src/components/Mesh.vue
+++ src/components/Mesh.vue
@@ -2,108 +2,71 @@
   <v-container fluid>
     <v-card>
       <v-container grid-list-md>
-        <v-row>
-          <v-col cols="3" md="2">
-            <v-text-field
-              label="Nodes size"
-              v-model.number="nodeSize"
-              min="10"
-              type="number"
-            ></v-text-field>
-          </v-col>
-          <v-col cols="3" md="2">
-            <v-text-field
-              label="Font size"
-              v-model.number="fontSize"
-              min="10"
-              type="number"
-            ></v-text-field>
-          </v-col>
-          <v-col cols="3" md="2">
-            <v-text-field
-              label="Distance"
-              v-model.number="force"
-              min="100"
-              type="number"
-            ></v-text-field>
-          </v-col>
-          <v-col cols="3" md="2">
-            <v-switch label="Show location" v-model="showLocation"></v-switch>
-          </v-col>
-          <v-col cols="5" md="6">
-            <v-btn color="success" @click="downloadSVG">Download SVG</v-btn>
-          </v-col>
-        </v-row>
+        <zwave-graph
+          id="mesh"
+          :nodes="nodes.filter(n => !n.failed)"
+          @node-click="nodeClick"
+        />
+
+        <div id="properties" draggable v-show="showProperties" class="details">
+          <v-icon
+            @click="showProperties = false"
+            style="cursor:pointer;position:absolute;right:10px;top:10px"
+            >clear</v-icon
+          >
+          <v-subheader>Node properties</v-subheader>
+          <v-list
+            v-if="selectedNode"
+            dense
+            style="min-width:300px;background:transparent"
+          >
+            <v-list-item>
+              <v-list-item-content>ID</v-list-item-content>
+              <v-list-item-content class="align-end">{{
+                selectedNode.data.id
+              }}</v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>Status</v-list-item-content>
+              <v-list-item-content class="align-end">{{
+                selectedNode.data.status
+              }}</v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>Code</v-list-item-content>
+              <v-list-item-content class="align-end">{{
+                selectedNode.data.productLabel
+              }}</v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>Product</v-list-item-content>
+              <v-list-item-content class="align-end">{{
+                selectedNode.data.productDescription
+              }}</v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>Manufacturer</v-list-item-content>
+              <v-list-item-content class="align-end">{{
+                selectedNode.data.manufacturer
+              }}</v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>Name</v-list-item-content>
+              <v-list-item-content class="align-end">{{
+                selectedNode.data.name
+              }}</v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>Location</v-list-item-content>
+              <v-list-item-content class="align-end">{{
+                selectedNode.data.loc
+              }}</v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </div>
       </v-container>
 
-      <d3-network
-        id="mesh"
-        ref="mesh"
-        :net-nodes="meshNodes"
-        :net-links="links"
-        :options="options"
-        :selection="selection"
-        @node-click="nodeClick"
-      />
-
-      <div id="properties" draggable v-show="showProperties" class="details">
-        <v-icon
-          @click="showProperties = false"
-          style="cursor:pointer;position:absolute;right:10px;top:10px"
-          >clear</v-icon
-        >
-        <v-subheader>Node properties</v-subheader>
-        <v-list
-          v-if="selectedNode"
-          dense
-          style="min-width:300px;background:transparent"
-        >
-          <v-list-item>
-            <v-list-item-content>ID</v-list-item-content>
-            <v-list-item-content class="align-end">{{
-              selectedNode.data.id
-            }}</v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>Status</v-list-item-content>
-            <v-list-item-content class="align-end">{{
-              selectedNode.data.status
-            }}</v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>Code</v-list-item-content>
-            <v-list-item-content class="align-end">{{
-              selectedNode.data.productLabel
-            }}</v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>Product</v-list-item-content>
-            <v-list-item-content class="align-end">{{
-              selectedNode.data.productDescription
-            }}</v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>Manufacturer</v-list-item-content>
-            <v-list-item-content class="align-end">{{
-              selectedNode.data.manufacturer
-            }}</v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>Name</v-list-item-content>
-            <v-list-item-content class="align-end">{{
-              selectedNode.data.name
-            }}</v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>Location</v-list-item-content>
-            <v-list-item-content class="align-end">{{
-              selectedNode.data.loc
-            }}</v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </div>
-
-      <v-speed-dial bottom fab right fixed v-model="fab">
+      <!-- <v-speed-dial bottom fab right fixed v-model="fab">
         <template v-slot:activator>
           <v-btn color="blue darken-2" dark fab hover v-model="fab">
             <v-icon v-if="fab">close</v-icon>
@@ -113,12 +76,24 @@
         <v-btn fab dark small color="green" @click="refresh">
           <v-icon>refresh</v-icon>
         </v-btn>
-      </v-speed-dial>
+      </v-speed-dial> -->
     </v-card>
   </v-container>
 </template>
+
+<style scoped>
+.details {
+  position: absolute;
+  top: 150px;
+  left: 30px;
+  background: #ccccccaa;
+  border: 2px solid black;
+  border-radius: 20px;
+}
+</style>
+
 <script>
-import D3Network from 'vue-d3-network'
+import ZwaveGraph from '@/components/custom/ZwaveGraph.vue'
 import { mapMutations, mapGetters } from 'vuex'
 
 import { socketEvents, inboundEvents as socketActions } from '@/plugins/socket'
@@ -129,70 +104,15 @@ export default {
     socket: Object
   },
   components: {
-    D3Network
+    ZwaveGraph
   },
   watch: {
-    meshNodes () {
+    nodes () {
       this.debounceRefresh()
     }
   },
   computed: {
-    ...mapGetters(['nodes']),
-    meshNodes () {
-      return this.activeNodes.map(n => this.convertNode(n))
-    },
-    activeNodes () {
-      return this.nodes.filter(n => n.id !== 0 && n.status !== 'Removed')
-    },
-    links () {
-      const links = []
-
-      for (const source of this.activeNodes) {
-        if (source.neighbors) {
-          for (const target of source.neighbors) {
-            // ensure target node exists
-            if (this.nodes[target] && this.nodes[target].status !== 'Removed') {
-              links.push({
-                sid: source.id,
-                tid: target,
-                _color: this.$vuetify.theme.dark ? 'white' : 'black'
-              })
-            }
-          }
-        }
-      }
-
-      return links
-    },
-    options () {
-      return {
-        canvas: false,
-        force: this.force,
-        offset: {
-          x: 0,
-          y: 0
-        },
-        nodeSize: this.nodeSize,
-        fontSize: this.fontSize,
-        linkWidth: 1,
-        nodeLabels: true,
-        linkLabels: false,
-        strLinks: true,
-        resizeListener: true
-      }
-    },
-    selection () {
-      const s = {
-        nodes: [],
-        links: []
-      }
-
-      if (this.selectedNode) {
-        s.nodes[this.selectedNode.id] = this.selectedNode
-      }
-
-      return s
-    }
+    ...mapGetters(['nodes'])
   },
   data () {
     return {
@@ -211,29 +131,6 @@ export default {
     nodeClick (e, node) {
       this.selectedNode = this.selectedNode === node ? null : node
       this.showProperties = !!this.selectedNode
-    },
-    downloadSVG () {
-      this.$refs.mesh.screenShot('myNetwork.svg', true, true)
-    },
-    convertNode (n) {
-      return {
-        id: n.id,
-        _cssClass: this.nodeClass(n),
-        name: this.nodeName(n),
-        status: n.status,
-        data: n
-      }
-    },
-    nodeName (n) {
-      if (n.data) n = n.data // works both with node object and mesh node object
-      const name = n.name || n.product || 'node ' + n.id
-      return name + (this.showLocation && n.loc ? ` (${n.loc})` : '')
-    },
-    nodeClass (n) {
-      if (n.id === 1) {
-        return 'controller'
-      }
-      return n.status.toLowerCase()
     },
     debounceRefresh () {
       if (this.refreshTimeout) {
--- src/main.js
+++ src/main.js
@@ -9,11 +9,8 @@ import store from './store'
 import vuetify from '@/plugins/vuetify' // path to vuetify export
 
 import 'axios-progress-bar/dist/nprogress.css'
-import 'vue-d3-network/dist/vue-d3-network.css'
-
 // Custom assets CSS JS
 require('./assets/css/my-progress.css')
-require('./assets/css/my-mesh.css')
 
 Vue.config.productionTip = false
 Vue.config.devtools = true
