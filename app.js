7d845e2ee1f52c1f2e8de796e53ff0781f601f15
Daniel Lando <daniel.sorridi@gmail.com>
2021-02-16 17:36:24 +0100

    fix: added missing rate limiter

--- app.js
+++ app.js
@@ -794,7 +794,7 @@ app.post('/api/store-multi', storeLimiter, isAuthenticated, function (
 })
 
 // update settings
-app.post('/api/settings', isAuthenticated, async function (req, res) {
+app.post('/api/settings', apisLimiter, isAuthenticated, async function (req, res) {
   try {
     if (restarting) {
       throw Error(
