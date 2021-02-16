34405d6d31121e721c4cb90cc23e20345b659d93
Daniel Lando <daniel.sorridi@gmail.com>
2021-02-16 11:45:23 +0100

    fix: removed credentials key and file from config

--- config/app.js
+++ config/app.js
@@ -3,8 +3,6 @@ const { joinPath } = require('../lib/utils')
 module.exports = {
   title: 'ZWave To MQTT',
   storeDir: process.env.STORE_DIR || joinPath(true, 'store'),
-  credentialsFile: 'credentials.json',
-  credentialsKey: 'credentials.key',
   defaultUser: 'admin',
   defaultPsw: 'zwave',
   sessionSecret:
