febf1756a3006ad1db7dd1d8f2b37aa873ab0cae
Daniel Lando <daniel.sorridi@gmail.com>
2021-02-18 09:49:49 +0100

    chore: automated release workflow using zwave-js bot

--- .github/bot-scripts/index.js
+++ .github/bot-scripts/index.js
@@ -1,6 +1,7 @@
 module.exports = {
 	checkAuthorized: (...args) => require("./checkAuthorized")(...args),
 	fixLintFeedback: (...args) => require("./fixLintFeedback")(...args),
+	getFixLintInfo: (...args) => require("./getFixLintInfo")(...args),
 	rebaseFeedback: (...args) => require("./rebaseFeedback")(...args),
 	renameCommitGetPRInfo: (...args) =>
 		require("./renameCommitGetPRInfo")(...args),
