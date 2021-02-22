990f1b62cee6d4e1f3da1c5c6135bcbebeadfd4a
Daniel Winks <daniel.winks@gmail.com>
2021-02-22 13:23:22 -0500

    Update HASS discovery for LZW42

--- hass/devices.js
+++ hass/devices.js
@@ -220,32 +220,37 @@ module.exports = {
     }
   ],
   '798-1-5': [
+
     // Inovelli LZW42 Multi-Color Bulb
     {
       type: 'light',
       object_id: 'rgbw_bulb',
-      values: ['38-0-currentValue', '38-0-targetValue', '51-1-0'], // FIXME: Handle color CC
+      values: ['38-0-currentValue', '38-0-targetValue', '51-0-currentColor', '51-0-targetColor'],
       discovery_payload: {
         state_topic: '38-0-currentValue',
         command_topic: '38-0-targetValue',
         on_command_type: 'brightness',
         brightness_state_topic: '38-0-currentValue',
         brightness_command_topic: '38-0-targetValue',
-        state_value_template: '{{ "OFF" if value_json.value == 0 else "ON" }}',
+        state_value_template: '{{ "on" if value_json.value|int > 0 else "0" }}',
         brightness_value_template: '{{ (value_json.value) | round(0) }}',
         brightness_scale: '99',
-        color_temp_state_topic: '51-1-0',
+        color_temp_state_topic: '51-0-currentColor',
         color_temp_command_template:
-          "{{ '#%02x%02x%02x%02x%02x' | format(0, 0, 0, (0.7349 * (value - 153)) | round(0), 255 - (0.7349 * (value - 153)) | round(0))}}",
-        color_temp_command_topic: '51-1-0',
+          "{{ {'warmWhite': ((0.7349 * (value - 153))|round(0)), 'coldWhite': (255 - (0.7349 * (value - 153))|round(0)), 'red': 255, 'green': 255, 'blue': 255}|to_json }}",
+        color_temp_command_topic: '51-0-targetColor',
         color_temp_value_template:
-          '{{ (((value_json.value[7:9] | int(0, 16)) / 0.7349 ) | round(0)) + 153 }}',
+          "{{ '%03d%03d' | format((value_json.value.warmWhite|int), (value_json.value.coldWhite|int)) }}",
         rgb_command_template:
-          "{{'#%02x%02x%02x%02x%02x' | format(red, green, blue,0,0)}}",
-        rgb_command_topic: '51-1-0',
-        rgb_state_topic: '51-1-0',
+          "{{ {'warmWhite': 0, 'coldWhite': 0, 'red': red, 'green': green, 'blue': blue}|to_json }}",
+        rgb_command_topic: '51-0-targetColor',
+        rgb_state_topic: '51-0-currentColor',
         rgb_value_template:
-          '{{ value_json.value[1:3] | int(0, 16) }},{{ value_json.value[3:5] | int(0, 16) }},{{ value_json.value[5:7] | int(0, 16) }}'
+          "{{ value_json.value.red }},{{ value_json.value.green }},{{ value_json.value.blue }}",
+        min_mireds: 153,
+        max_mireds: 500,
+        payload_on: "on",
+        payload_off: "0"
       }
     }
   ],
