a213b25cc340a9bd8c69dcf7a027662e3811081d
Daniel Lando <daniel.sorridi@gmail.com>
2021-02-24 17:16:40 +0100

    feat: plugins improved support (#688)
    With this PR users will have more power when developing custom plugins. More info in [docs](https://zwave-js.github.io/zwavejs2mqtt/#/guide/plugins)
    
    BREAKING CHANGE: `plugins` are now stored in an array of `strings` on settings `gateway` prop instead of `zwave`

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -26,6 +26,18 @@
                         ></v-switch>
                       </v-col>
                       <v-col cols="12" sm="6" md="4">
+                        <v-combobox
+                          hint="You can select a plugin from the list or write the path to your custom plugin and press enter"
+                          persistent-hint
+                          label="Plugins"
+                          :items="['@varet/zj2m-prom-exporter']"
+                          multiple
+                          chips
+                          deletable-chips
+                          v-model="newGateway.plugins"
+                        ></v-combobox>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="4">
                         <v-switch
                           hint="Enable logging"
                           persistent-hint
