de8847bc7eec39f32d76c4d37c5a1c0f6545eddd
Daniel Lando <daniel.sorridi@gmail.com>
2021-03-10 08:57:38 +0100

    fix: lint issues and missing fallbacks

--- types/index.d.ts
+++ types/index.d.ts
@@ -228,6 +228,10 @@ export interface MqttClient extends EventEmitter {
     listener: (parts: string[], payload: any) => void
   ): this
   on(
+    event: 'multicastRequest',
+    listener: (payload: any) => void
+  ): this
+  on(
     event: 'apiCall',
     listener: (topic: string, apiNema: string, payload: any) => void
   ): this
@@ -390,7 +394,13 @@ export interface ZwaveClient extends EventEmitter {
     apiName: string,
     ...args: any
   ): Promise<{ success: boolean; message: string; result: any; args: any[] }>
-  writeValue(valueId: Z2MValueId, value: number | string): Promise<void>
+  writeBroadcast(valueId: Z2MValueId, value: unknown): Promise<void>
+  writeMulticast(
+    nodes: number[],
+    valueId: Z2MValueId,
+    value: unknown
+  ): Promise<void>
+  writeValue(valueId: Z2MValueId, value: unknown): Promise<void>
   sendCommand(
     ctx: { nodeId: number; endpoint: number; commandClass: number },
     command: string,
