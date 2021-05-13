f668ea4dba0420d28447930fad8ba4f7eb20f16e
Tommi Pääkkö <tommi.paakko@symbio.com>
2021-05-13 12:22:59 +0300

    Add support for dotenv file (.env) in development

--- config/index.js
+++ config/index.js
@@ -3,6 +3,7 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require('path')
+require('dotenv').config()
 
 const proxyScheme = process.env.SERVER_SSL ? 'https' : 'http'
 const proxyWebSocketScheme = process.env.SERVER_SSL ? 'wss' : 'ws'
