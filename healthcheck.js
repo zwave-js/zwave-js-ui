fe4c508aa84203d5e7a22957f66cf4bf6abebc9f
Daniel Lando <daniel.sorridi@gmail.com>
2021-05-13 11:05:48 +0200

    chore(docker): add HEALTHCHECK
    Fixes #1210

---
+++ healthcheck.js
@@ -0,0 +1,39 @@
+const http = require('http')
+
+const availableClients = {
+  all: '',
+  zwave: '/zwave',
+  mqtt: '/mqtt'
+}
+
+let clientCheck = availableClients.all
+
+if (process.env.HEALTH_CLIENT) {
+  for (const c in availableClients) {
+    if (c === process.env.HEALTH_CLIENT) {
+      clientCheck = availableClients[c]
+    }
+  }
+}
+
+const options = {
+  port: require('./config/app.js').port,
+  timeout: 2000,
+  path: '/health' + clientCheck
+}
+
+const request = http.request(options, res => {
+  console.log(`STATUS: ${res.statusCode}`)
+  if (res.statusCode === 200) {
+    process.exit(0)
+  } else {
+    process.exit(1)
+  }
+})
+
+request.on('error', function (err) {
+  console.log('ERROR: ' + err.message)
+  process.exit(1)
+})
+
+request.end()
