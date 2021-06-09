7c74b8805edb1a5d703cd0379663b6f18362eecc
Daniel Lando <daniel.sorridi@gmail.com>
2021-06-09 15:51:10 +0200

    fix: add custom plugin interface

--- lib/CustomPlugin.ts
+++ lib/CustomPlugin.ts
@@ -1,1 +1,22 @@
-export default interface CustomPlugin {}
+import { Router } from 'express'
+import MqttClient from './MqttClient'
+import { ModuleLogger } from './logger'
+import ZwaveClient from './ZwaveClient'
+
+export type PluginContext = {
+	zwave: ZwaveClient
+	mqtt: MqttClient
+	app: Router
+	logger: ModuleLogger
+}
+
+export interface CustomPlugin {
+	zwave: ZwaveClient
+	mqtt: MqttClient
+	app: Router
+	logger: ModuleLogger
+
+	new (context: PluginContext)
+
+	destroy(): Promise<void>
+}
