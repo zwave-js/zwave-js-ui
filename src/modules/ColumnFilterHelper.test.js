5021811944b87ffaf820858d0a18ecad9fad9b36
Daniel Lando <daniel.sorridi@gmail.com>
2021-06-10 10:41:25 +0200

    refactor: modules tests

--- src/modules/ColumnFilterHelper.test.js
+++ src/modules/ColumnFilterHelper.test.js
@@ -1,22 +1,22 @@
-import chai from 'chai'
+import { expect } from 'chai'
 import { ColumnFilterHelper } from './ColumnFilterHelper'
 
 describe('ColumnFilterHelper', () => {
 	describe('#defaultFilter', () => {
 		it('returns the default filter for the given column type', () => {
-			chai.expect(ColumnFilterHelper.defaultFilter('boolean')).to.eql({
+			expect(ColumnFilterHelper.defaultFilter('boolean')).to.eql({
 				boolValue: null,
 			})
-			chai.expect(ColumnFilterHelper.defaultFilter('date')).to.eql({
+			expect(ColumnFilterHelper.defaultFilter('date')).to.eql({
 				from: null,
 				to: null,
 			})
-			chai.expect(ColumnFilterHelper.defaultFilter('number')).to.eql({
+			expect(ColumnFilterHelper.defaultFilter('number')).to.eql({
 				min: null,
 				max: null,
 				values: [],
 			})
-			chai.expect(ColumnFilterHelper.defaultFilter('string')).to.eql({
+			expect(ColumnFilterHelper.defaultFilter('string')).to.eql({
 				match: '',
 				values: [],
 			})
@@ -25,19 +25,19 @@ describe('ColumnFilterHelper', () => {
 
 	describe('#filterProps', () => {
 		it('returns the list of filter props for the given column type', () => {
-			chai.expect(ColumnFilterHelper.filterProps('boolean')).to.eql([
+			expect(ColumnFilterHelper.filterProps('boolean')).to.eql([
 				'boolValue',
 			])
-			chai.expect(ColumnFilterHelper.filterProps('date')).to.eql([
+			expect(ColumnFilterHelper.filterProps('date')).to.eql([
 				'from',
 				'to',
 			])
-			chai.expect(ColumnFilterHelper.filterProps('number')).to.eql([
+			expect(ColumnFilterHelper.filterProps('number')).to.eql([
 				'min',
 				'max',
 				'values',
 			])
-			chai.expect(ColumnFilterHelper.filterProps('string')).to.eql([
+			expect(ColumnFilterHelper.filterProps('string')).to.eql([
 				'match',
 				'values',
 			])
--- src/modules/NodeCollection.test.js
+++ src/modules/NodeCollection.test.js
@@ -1,11 +1,11 @@
-import chai from 'chai'
+import { expect } from 'chai'
 import { NodeCollection } from './NodeCollection'
 
 describe('NodeCollection', () => {
 	describe('#constructor', () => {
 		it('uses the nodes passed in as the collection nodes', () => {
 			const collection = new NodeCollection([{ id: 1 }])
-			chai.expect(collection.nodes).to.eql([{ id: 1 }])
+			expect(collection.nodes).to.eql([{ id: 1 }])
 		})
 	})
 	describe('#filter', () => {
@@ -18,7 +18,7 @@ describe('NodeCollection', () => {
 				{ id: 4 },
 			])
 			const filtered = collection.filter('id', isOdd)
-			chai.expect(filtered.nodes).to.eql([{ id: 1 }, { id: 3 }])
+			expect(filtered.nodes).to.eql([{ id: 1 }, { id: 3 }])
 		})
 		it('returns nodes with any of the properties matching the filter', () => {
 			const collection = new NodeCollection([
@@ -28,7 +28,7 @@ describe('NodeCollection', () => {
 				{ id: 4, value: 2 },
 			])
 			const filtered = collection.filter(['id', 'value'], isOdd)
-			chai.expect(filtered.nodes).to.eql([
+			expect(filtered.nodes).to.eql([
 				{ id: 1, value: 2 },
 				{ id: 2, value: 1 },
 				{ id: 3, value: 2 },
@@ -51,7 +51,7 @@ describe('NodeCollection', () => {
 				{ id: '300' },
 			])
 			const filtered = collection.contains('id', '10')
-			chai.expect(filtered.nodes).to.eql([{ id: 100 }, { id: '210' }])
+			expect(filtered.nodes).to.eql([{ id: 100 }, { id: '210' }])
 		})
 		it('matches values over multiple properties', () => {
 			const collection = new NodeCollection([
@@ -61,7 +61,7 @@ describe('NodeCollection', () => {
 				{ id: '300', name: 'fazuoli' },
 			])
 			const filtered = collection.contains(['id', 'name'], '10')
-			chai.expect(filtered.nodes).to.eql([
+			expect(filtered.nodes).to.eql([
 				{ id: 100, name: 'sample' },
 				{ id: '210', name: 'trinity' },
 				{ id: 20, name: '10 packs' },
@@ -69,14 +69,11 @@ describe('NodeCollection', () => {
 		})
 		it('is case insensitive by default', () => {
 			const filtered = stringCollection.contains('id', 'piPPo')
-			chai.expect(filtered.nodes).to.eql([
-				{ id: 'pippo' },
-				{ id: 'PipPo' },
-			])
+			expect(filtered.nodes).to.eql([{ id: 'pippo' }, { id: 'PipPo' }])
 		})
 		it('accepts a case sensitive flag', () => {
 			const filtered = stringCollection.contains('id', 'PipPo', true)
-			chai.expect(filtered.nodes).to.eql([{ id: 'PipPo' }])
+			expect(filtered.nodes).to.eql([{ id: 'PipPo' }])
 		})
 	})
 	describe('#equals', () => {
@@ -88,7 +85,7 @@ describe('NodeCollection', () => {
 				{ id: '20' },
 			])
 			const filtered = collection.equals('id', 10)
-			chai.expect(filtered.nodes).to.eql([{ id: 10 }])
+			expect(filtered.nodes).to.eql([{ id: 10 }])
 		})
 		it('works over multiple properties', () => {
 			const collection = new NodeCollection([
@@ -98,7 +95,7 @@ describe('NodeCollection', () => {
 				{ id: '20', sample: 10 },
 			])
 			const filtered = collection.equals(['id', 'sample'], 10)
-			chai.expect(filtered.nodes).to.eql([
+			expect(filtered.nodes).to.eql([
 				{ id: 10, sample: '20' },
 				{ id: '20', sample: 10 },
 			])
@@ -113,7 +110,7 @@ describe('NodeCollection', () => {
 				{ id: '20' },
 			])
 			const filtered = collection.equalsAny('id', [])
-			chai.expect(filtered.nodes).to.eql([
+			expect(filtered.nodes).to.eql([
 				{ id: 10 },
 				{ id: '10' },
 				{ id: 20 },
@@ -128,7 +125,7 @@ describe('NodeCollection', () => {
 				{ id: '20' },
 			])
 			const filtered = collection.equalsAny('id', [10, '20'])
-			chai.expect(filtered.nodes).to.eql([{ id: 10 }, { id: '20' }])
+			expect(filtered.nodes).to.eql([{ id: 10 }, { id: '20' }])
 		})
 		it('works over multiple properties', () => {
 			const collection = new NodeCollection([
@@ -138,7 +135,7 @@ describe('NodeCollection', () => {
 				{ id: '20', sample: 'zdub' },
 			])
 			const filtered = collection.equalsAny(['id', 'sample'], [10, '20'])
-			chai.expect(filtered.nodes).to.eql([
+			expect(filtered.nodes).to.eql([
 				{ id: 10, sample: 20 },
 				{ id: '10', sample: '20' },
 				{ id: '20', sample: 'zdub' },
@@ -157,7 +154,7 @@ describe('NodeCollection', () => {
 				undefined,
 				undefined
 			)
-			chai.expect(filtered.nodes).to.eql([
+			expect(filtered.nodes).to.eql([
 				{ id: 10, sample: 10 },
 				{ id: 20, sample: 20 },
 				{ id: 30, sample: 30 },
@@ -170,7 +167,7 @@ describe('NodeCollection', () => {
 				{ id: 30, sample: 30 },
 			])
 			const filtered = collection.betweenNumber('id', null, null)
-			chai.expect(filtered.nodes).to.eql([
+			expect(filtered.nodes).to.eql([
 				{ id: 10, sample: 10 },
 				{ id: 20, sample: 20 },
 				{ id: 30, sample: 30 },
@@ -183,7 +180,7 @@ describe('NodeCollection', () => {
 				{ id: 30, sample: 30 },
 			])
 			const filtered = collection.betweenNumber('id', 20, null)
-			chai.expect(filtered.nodes).to.eql([
+			expect(filtered.nodes).to.eql([
 				{ id: 20, sample: 20 },
 				{ id: 30, sample: 30 },
 			])
@@ -195,7 +192,7 @@ describe('NodeCollection', () => {
 				{ id: 30, sample: 30 },
 			])
 			const filtered = collection.betweenNumber('id', null, 20)
-			chai.expect(filtered.nodes).to.eql([
+			expect(filtered.nodes).to.eql([
 				{ id: 10, sample: 10 },
 				{ id: 20, sample: 20 },
 			])
@@ -207,7 +204,7 @@ describe('NodeCollection', () => {
 				{ id: 30, sample: 30 },
 			])
 			const filtered = collection.betweenNumber('id', 15, 25)
-			chai.expect(filtered.nodes).to.eql([{ id: 20, sample: 20 }])
+			expect(filtered.nodes).to.eql([{ id: 20, sample: 20 }])
 		})
 	})
 	describe('#betweenDate', () => {
@@ -222,7 +219,7 @@ describe('NodeCollection', () => {
 				undefined,
 				undefined
 			)
-			chai.expect(filtered.nodes).to.eql([
+			expect(filtered.nodes).to.eql([
 				{ id: 10, lastActive: new Date(2020, 11, 9, 0, 0) },
 				{ id: 20, lastActive: new Date(2020, 11, 10, 0, 0) },
 				{ id: 30, lastActive: new Date(2020, 11, 11, 0, 0) },
@@ -235,7 +232,7 @@ describe('NodeCollection', () => {
 				{ id: 30, lastActive: new Date(2020, 11, 11, 0, 0) },
 			])
 			const filtered = collection.betweenDate('lastActive', null, null)
-			chai.expect(filtered.nodes).to.eql([
+			expect(filtered.nodes).to.eql([
 				{ id: 10, lastActive: new Date(2020, 11, 9, 0, 0) },
 				{ id: 20, lastActive: new Date(2020, 11, 10, 0, 0) },
 				{ id: 30, lastActive: new Date(2020, 11, 11, 0, 0) },
@@ -252,7 +249,7 @@ describe('NodeCollection', () => {
 				new Date(2020, 11, 10, 0, 0),
 				null
 			)
-			chai.expect(filtered.nodes).to.eql([
+			expect(filtered.nodes).to.eql([
 				{ id: 20, lastActive: new Date(2020, 11, 10, 0, 0) },
 				{ id: 30, lastActive: new Date(2020, 11, 11, 0, 0) },
 			])
@@ -268,7 +265,7 @@ describe('NodeCollection', () => {
 				null,
 				new Date(2020, 11, 10, 0, 0)
 			)
-			chai.expect(filtered.nodes).to.eql([
+			expect(filtered.nodes).to.eql([
 				{ id: 10, lastActive: new Date(2020, 11, 9, 0, 0) },
 				{ id: 20, lastActive: new Date(2020, 11, 10, 0, 0) },
 			])
@@ -284,7 +281,7 @@ describe('NodeCollection', () => {
 				new Date(2020, 11, 9, 12, 0),
 				new Date(2020, 11, 10, 12, 0)
 			)
-			chai.expect(filtered.nodes).to.eql([
+			expect(filtered.nodes).to.eql([
 				{ id: 20, lastActive: new Date(2020, 11, 10, 0, 0) },
 			])
 		})
