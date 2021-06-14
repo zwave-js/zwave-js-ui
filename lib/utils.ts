4314d465f4d8c18677fe3ed4ba2ed063e873e2cc
Daniel Lando <daniel.sorridi@gmail.com>
2021-06-14 15:05:53 +0200

    fix: better mixin function

--- lib/utils.ts
+++ lib/utils.ts
@@ -24,15 +24,39 @@ export interface ErrnoException extends Error {
 	stack?: string
 }
 
-export function applyMixin(derivedCtor: any, constructor: any) {
-	Object.getOwnPropertyNames(constructor.prototype).forEach((name) => {
-		Object.defineProperty(
-			derivedCtor.prototype,
-			name,
-			Object.getOwnPropertyDescriptor(constructor.prototype, name) ||
-				Object.create(null)
-		)
-	})
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type Constructor<T = {}> = new (...args: any[]) => T
+
+export function applyMixin(
+	target: Constructor,
+	mixin: Constructor,
+	includeConstructor = false
+): void {
+	// Figure out the inheritance chain of the mixin
+	const inheritanceChain: Constructor[] = [mixin]
+	// eslint-disable-next-line no-constant-condition
+	while (true) {
+		const current = inheritanceChain[0]
+		const base = Object.getPrototypeOf(current)
+		if (base?.prototype) {
+			inheritanceChain.unshift(base)
+		} else {
+			break
+		}
+	}
+	for (const ctor of inheritanceChain) {
+		for (const prop of Object.getOwnPropertyNames(ctor.prototype)) {
+			// Do not override the constructor
+			if (includeConstructor || prop !== 'constructor') {
+				Object.defineProperty(
+					target.prototype,
+					prop,
+					Object.getOwnPropertyDescriptor(ctor.prototype, prop) ??
+						Object.create(null)
+				)
+			}
+		}
+	}
 }
 
 /**
