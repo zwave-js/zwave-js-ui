e4dd111faf53b9727a8a2fba43e0fb9ae4297830
Daniel Lando <daniel.sorridi@gmail.com>
2021-09-28 09:34:49 +0200

    chore: try fix icons path

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -72,14 +72,23 @@ module.exports = {
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         type: 'asset/resource',
+        generator: {
+          filename: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
         type: 'asset/resource',
+        generator: {
+          filename: utils.assetsPath('media/[name].[hash:7].[ext]')
+        }
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        type: 'asset/resource'
+        type: 'asset/resource',
+        generator: {
+          filename: utils.assetsPath('fonts/[name].[hash:7].[ext]'),
+        }
       },
       {
         test: /\.s(c|a)ss$/,
