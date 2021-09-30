84fa7b8bc0d142fffeb1350bba0a721d6aea8f42
Daniel Lando <daniel.sorridi@gmail.com>
2021-09-30 16:14:31 +0200

    Revert "chore: bump css-loader@6.3.0 and fix breaking changes"
    This reverts commit c2bd96784ab34a48757f1cd1a02c805e10b812cc.

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -5,7 +5,7 @@ const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
-function resolve(dir) {
+function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
@@ -71,23 +71,27 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        type: 'asset/resource',
-        generator: {
-          filename: utils.assetsPath('img/[name].[hash:7].[ext]')
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        type: 'asset/resource',
-        generator: {
-          filename: utils.assetsPath('media/[name].[hash:7].[ext]')
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('media/[name].[hash:7].[ext]')
         }
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        type: 'asset/resource',
-        generator: {
-          filename: utils.assetsPath('../fonts/[name].[hash:7].[ext]'),
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]'),
+          publicPath: '../..'
         }
       },
       {
