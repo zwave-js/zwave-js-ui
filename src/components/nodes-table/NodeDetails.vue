671791c4f8ec77e540c7dd50a58a644b59fe3d4a
floutchito <floutchito@gmail.com>
2022-03-20 15:06:44 +0100

    feat(ui): Reworked Node views
    Pulled Ping / Advanced node actions out of NodeDetails
    Using vertical tabs
    Unwrapped manual instructions from tabs
    Flattened values expansion panels

--- src/components/nodes-table/NodeDetails.vue
+++ src/components/nodes-table/NodeDetails.vue
@@ -1,37 +1,6 @@
 <template>
-	<v-container grid-list-md>
-		<v-row class="mt-5" align="center">
-			<v-subheader class="title">Device ID </v-subheader>
-			<span class="subtitle font-weight-bold">{{
-				`${node.deviceId} (${node.hexId})`
-			}}</span>
-			<v-icon @click="openLink(node.dbLink)" class="ml-2"
-				>ios_share</v-icon
-			>
-		</v-row>
-
-		<v-row>
-			<v-col cols="8" style="max-width: 300px">
-				<v-btn
-					dark
-					color="primary"
-					@click.stop="apiRequest('pingNode', [node.id])"
-					depressed
-				>
-					Ping
-				</v-btn>
-				<v-btn
-					dark
-					color="green"
-					depressed
-					@click="advancedShowDialog = true"
-				>
-					Advanced
-				</v-btn>
-			</v-col>
-		</v-row>
-
-		<v-row justify="start">
+	<v-container fluid>
+		<v-row no-gutters>
 			<v-sheet outlined rounded>
 				<statistics-card title="Statistics" :node="node" />
 			</v-sheet>
@@ -92,7 +61,12 @@
 		<v-row>
 			<v-subheader class="title">Values</v-subheader>
 
-			<v-expansion-panels accordion multiple>
+			<v-expansion-panels
+				class="expansion-panels-outlined"
+				accordion
+				multiple
+				flat
+			>
 				<v-expansion-panel
 					v-for="(group, className) in commandGroups"
 					:key="className"
@@ -111,126 +85,115 @@
 											group[0].commandClass,
 										])
 									"
+									color="primary"
+									outlined
 									x-small
 								>
 									Refresh
+									<v-icon x-small right>refresh</v-icon>
 								</v-btn>
 							</v-col>
 						</v-row>
 					</v-expansion-panel-header>
 					<v-expansion-panel-content>
-						<v-card flat>
-							<v-card-text>
+						<v-row>
+							<v-col
+								cols="12"
+								v-for="(v, index) in group"
+								:key="index"
+								sm="6"
+								md="4"
+							>
+								<ValueID
+									@updateValue="updateValue"
+									v-model="group[index]"
+								></ValueID>
+							</v-col>
+						</v-row>
+						<v-row>
+							<v-col
+								v-if="className.startsWith('Configuration')"
+								cols="12"
+								sm="6"
+								md="4"
+								style="padding-left: 0"
+							>
+								<v-subheader class="valueid-label"
+									>Custom Configuration
+								</v-subheader>
 								<v-row>
-									<v-col
-										cols="12"
-										v-for="(v, index) in group"
-										:key="index"
-										sm="6"
-										md="4"
-									>
-										<ValueID
-											@updateValue="updateValue"
-											v-model="group[index]"
-										></ValueID>
+									<v-col cols="3">
+										<v-text-field
+											label="Parameter"
+											v-model.number="configCC.parameter"
+										/>
+									</v-col>
+									<v-col cols="3">
+										<v-select
+											label="Size"
+											:items="[1, 2, 3, 4]"
+											v-model.number="configCC.valueSize"
+										/>
+									</v-col>
+									<v-col cols="3">
+										<v-text-field
+											label="Value"
+											v-model.number="configCC.value"
+										/>
+									</v-col>
+									<v-col cols="3">
+										<v-btn
+											width="60px"
+											@click.stop="
+												apiRequest('sendCommand', [
+													{
+														nodeId: node.id,
+														commandClass: 112,
+													},
+													'get',
+													[configCC.parameter],
+												])
+											"
+											color="green"
+											x-small
+										>
+											GET
+										</v-btn>
+										<v-btn
+											width="60px"
+											@click.stop="
+												apiRequest('sendCommand', [
+													{
+														nodeId: node.id,
+														commandClass: 112,
+													},
+													'set',
+													[
+														configCC.parameter,
+														configCC.value,
+														configCC.valueSize,
+													],
+												])
+											"
+											color="primary"
+											x-small
+										>
+											SET
+										</v-btn>
 									</v-col>
 								</v-row>
-								<v-col
-									v-if="className.startsWith('Configuration')"
-									cols="12"
-									sm="6"
-									md="4"
-									style="padding-left: 0"
-								>
-									<v-subheader class="valueid-label"
-										>Custom Configuration
-									</v-subheader>
-									<v-row>
-										<v-col cols="3">
-											<v-text-field
-												label="Parameter"
-												v-model.number="
-													configCC.parameter
-												"
-											/>
-										</v-col>
-										<v-col cols="3">
-											<v-select
-												label="Size"
-												:items="[1, 2, 3, 4]"
-												v-model.number="
-													configCC.valueSize
-												"
-											/>
-										</v-col>
-										<v-col cols="3">
-											<v-text-field
-												label="Value"
-												v-model.number="configCC.value"
-											/>
-										</v-col>
-										<v-col cols="3">
-											<v-btn
-												width="60px"
-												@click.stop="
-													apiRequest('sendCommand', [
-														{
-															nodeId: node.id,
-															commandClass: 112,
-														},
-														'get',
-														[configCC.parameter],
-													])
-												"
-												color="green"
-												x-small
-											>
-												GET
-											</v-btn>
-											<v-btn
-												width="60px"
-												@click.stop="
-													apiRequest('sendCommand', [
-														{
-															nodeId: node.id,
-															commandClass: 112,
-														},
-														'set',
-														[
-															configCC.parameter,
-															configCC.value,
-															configCC.valueSize,
-														],
-													])
-												"
-												color="primary"
-												x-small
-											>
-												SET
-											</v-btn>
-										</v-col>
-									</v-row>
-								</v-col>
-							</v-card-text>
-						</v-card>
+							</v-col>
+						</v-row>
 					</v-expansion-panel-content>
 					<v-divider></v-divider>
 				</v-expansion-panel>
 			</v-expansion-panels>
 		</v-row>
-
-		<DialogAdvanced
-			v-model="advancedShowDialog"
-			@close="advancedShowDialog = false"
-			:actions="actions"
-			@action="nodeAction"
-		/>
 	</v-container>
 </template>
 
 <script>
 import ValueID from '@/components/ValueId'
-import DialogAdvanced from '@/components/dialogs/DialogAdvanced'
 
 import { inboundEvents as socketActions } from '@/plugins/socket'
 import { mapMutations, mapGetters } from 'vuex'
@@ -245,7 +208,6 @@ export default {
 	},
 	components: {
 		ValueID,
-		DialogAdvanced,
 		StatisticsCard,
 	},
 	data() {
@@ -255,134 +217,11 @@ export default {
 			options: {},
 			newName: this.node.name,
 			newLoc: this.node.loc,
-			advancedShowDialog: false,
 			configCC: {
 				value: 0,
 				valueSize: 1,
 				parameter: 1,
 			},
-			actions: [
-				{
-					text: 'Export json',
-					options: [{ name: 'Export', action: 'exportNode' }],
-					icon: 'get_app',
-					desc: 'Export this node in a json file',
-				},
-				{
-					text: 'Clear Retained',
-					options: [
-						{
-							name: 'Clear',
-							action: 'removeNodeRetained',
-							args: {
-								mqtt: true,
-								confirm:
-									'Are you sure you want to remove all retained messages?',
-							},
-						},
-					],
-					icon: 'clear',
-					desc: 'All retained messages of this node will be removed from broker',
-				},
-				{
-					text: 'Update topics',
-					options: [
-						{
-							name: 'Update',
-							action: 'updateNodeTopics',
-							args: {
-								mqtt: true,
-								confirm:
-									'Are you sure you want to update all topics?',
-							},
-						},
-					],
-					icon: 'update',
-					desc: 'Update all node topics. Useful when name/location has changed',
-				},
-				{
-					text: 'Firmware update',
-					options: [
-						{ name: 'Begin', action: 'beginFirmwareUpdate' },
-						{ name: 'Abort', action: 'abortFirmwareUpdate' },
-					],
-					icon: 'update',
-					desc: 'Start/Stop a firmware update',
-				},
-				{
-					text: 'Heal Node',
-					options: [
-						{
-							name: 'Heal',
-							action: 'healNode',
-							args: {
-								confirm:
-									'Healing a node causes a lot of traffic, can take minutes up to hours and you can expect degraded performance while it is going on',
-							},
-						},
-					],
-					icon: 'healing',
-					desc: 'Force nodes to establish better connections to the controller',
-				},
-				{
-					text: 'Refresh Values',
-					options: [
-						{
-							name: 'Refresh',
-							action: 'refreshValues',
-							args: {
-								confirm:
-									'Are you sure you want to refresh values of this node? This action increases network traffic',
-							},
-						},
-					],
-					icon: 'cached',
-					desc: 'Update all CC values and metadata. Use only when many values seems stale',
-				},
-				{
-					text: 'Re-interview Node',
-					options: [
-						{
-							name: 'Interview',
-							action: 'refreshInfo',
-						},
-					],
-					icon: 'history',
-					desc: 'Clear all info about this node and make a new full interview. Use when the node has wrong or missing capabilities',
-				},
-				{
-					text: 'Failed Nodes',
-					options: [
-						{ name: 'Check', action: 'isFailedNode' },
-						{ name: 'Remove', action: 'removeFailedNode' },
-					],
-					icon: 'dangerous',
-					desc: 'Manage nodes that are dead and/or marked as failed with the controller',
-				},
-				{
-					text: 'Associations',
-					options: [
-						{
-							name: 'Clear',
-							action: 'removeAllAssociations',
-							args: {
-								confirm:
-									"This action will remove all associations of this node. This will also clear lifeline association with controller node, the node won't report state changes until that is set up again",
-							},
-						},
-						{
-							name: 'Remove',
-							action: 'removeNodeFromAllAssociations',
-							args: {
-								confirm:
-									'All direct associations to this node will be removed. Battery-powered nodes need to be woken up to edit their associations.',
-							},
-						},
-					],
-					icon: 'link_off',
-					desc: 'Clear all node associations / Remove node from all associations',
-				},
-			],
 		}
 	},
 	computed: {
@@ -429,18 +268,6 @@ export default {
 	},
 	methods: {
 		...mapMutations(['showSnackbar']),
-		nodeAction(action, args = {}) {
-			if (action === 'exportNode') {
-				this.exportNode()
-			} else if (args.mqtt) {
-				this.sendMqttAction(action, args.confirm)
-			} else {
-				this.$emit('action', action, { ...args, nodeId: this.node.id })
-			}
-		},
-		openLink(link) {
-			window.open(link, '_blank')
-		},
 		apiRequest(apiName, args) {
 			if (this.socket.connected) {
 				const data = {
@@ -551,4 +378,8 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.expansion-panels-outlined {
+	border: 1px solid rgba(0, 0, 0, 0.12);
+}
+</style>
