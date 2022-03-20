b0480f8bb812561744e94ea26b57424f1959b4b5
floutchito <floutchito@gmail.com>
2022-03-20 11:22:28 +0100

    feat(ui): Reworked Store view, two-panes layout using full available space
    Removed wrapping VCard
    Positioned file browser sticky to the top of the view
    Moved file selection VSpeedDial to file browser view
    
    Positioned file content actions sticky to the bottom of the file content view
    
    Vertically centered placeholder text and progress
    Small typographic styling

--- src/components/Store.vue
+++ src/components/Store.vue
@@ -1,114 +1,146 @@
 <template>
-	<v-container fluid>
-		<v-card height="800" style="margin-top: 30px; overflow: hidden">
-			<v-row class="pa-4 full-height" justify="space-between">
-				<v-col class="scroll" cols="5">
-					<v-treeview
-						v-if="!loadingStore"
-						:active.sync="active"
-						v-model="selectedFiles"
-						:items="items"
-						activatable
-						open-all
-						selectable
-						item-key="path"
-						open-on-click
-						return-object
-					>
-						<template v-slot:prepend="{ item, open }">
-							<v-icon color="#FFC107" v-if="!item.ext">
-								{{ open ? 'folder_open' : 'folder' }}
-							</v-icon>
-							<v-icon color="blue" v-else> text_snippet </v-icon>
-						</template>
-						<template v-slot:label="{ item }">
-							<span>{{ item.name }} </span>
-							<div class="caption grey--text">
-								{{ item.size !== 'n/a' ? item.size : '' }}
-							</div>
-						</template>
-						<template v-slot:append="{ item }">
-							<v-row justify-end class="ma-1">
-								<v-icon
-									v-if="item.children"
-									@click.stop="writeFile(item.path, true)"
-									color="yellow"
-									>create_new_folder</v-icon
-								>
-								<v-icon
-									v-if="item.children"
-									@click.stop="writeFile(item.path, false)"
-									color="primary"
-									>post_add</v-icon
-								>
-								<v-icon
-									v-if="!item.isRoot"
-									@click.stop="deleteFile(item)"
-									color="red"
-									>delete</v-icon
-								>
-							</v-row>
-						</template>
-					</v-treeview>
-					<div v-else>
-						<v-progress-circular
-							indeterminate
-							color="primary"
-							style="align-self: center"
-						></v-progress-circular>
-					</div>
-				</v-col>
-
-				<v-divider vertical></v-divider>
-
-				<v-col class="text-center no-scroll full-height">
-					<div
-						v-if="!selected || !selected.ext"
-						class="
-							title
-							grey--text
-							text--lighten-1
-							font-weight-light
-						"
+	<v-container class="full-height pa-0" fluid>
+		<v-row no-gutters>
+			<v-col
+				class="sticky-top fill-remaining-space overflow-y-auto"
+				cols="6"
+				md="5"
+				lg="4"
+				xl="3"
+			>
+				<v-treeview
+					v-if="!loadingStore"
+					:active.sync="active"
+					v-model="selectedFiles"
+					:items="items"
+					activatable
+					open-all
+					selectable
+					item-key="path"
+					open-on-click
+					return-object
+				>
+					<template v-slot:prepend="{ item, open }">
+						<v-icon color="#FFC107" v-if="!item.ext">
+							{{ open ? 'folder_open' : 'folder' }}
+						</v-icon>
+						<v-icon color="blue" v-else> text_snippet </v-icon>
+					</template>
+					<template v-slot:label="{ item }">
+						<span class="subtitle-2">{{ item.name }}</span>
+						<div class="caption grey--text">
+							{{ item.size !== 'n/a' ? item.size : '' }}
+						</div>
+					</template>
+					<template v-slot:append="{ item }">
+						<v-row justify-end class="ma-1">
+							<v-icon
+								v-if="item.children"
+								@click.stop="writeFile(item.path, true)"
+								color="yellow"
+								>create_new_folder</v-icon
+							>
+							<v-icon
+								v-if="item.children"
+								@click.stop="writeFile(item.path, false)"
+								color="primary"
+								>post_add</v-icon
+							>
+							<v-icon
+								v-if="!item.isRoot"
+								@click.stop="deleteFile(item)"
+								color="red"
+								>delete</v-icon
+							>
+						</v-row>
+					</template>
+				</v-treeview>
+				<div v-else>
+					<v-progress-circular
+						indeterminate
+						color="primary"
 						style="align-self: center"
-					>
-						Select a file
-					</div>
-					<div
-						v-else-if="loadingFile"
-						class="
-							title
-							grey--text
-							text--lighten-1
-							font-weight-light
-						"
-						style="align-self: center"
-					>
-						<v-progress-circular
-							indeterminate
-							color="primary"
-							style="align-self: center"
-						></v-progress-circular>
-					</div>
-					<v-card
-						class="no-scroll full-height"
-						v-else
-						:key="selected.path"
-						flat
-					>
-						<v-card-text
-							class="no-scroll"
-							style="height: calc(100% - 50px)"
+					></v-progress-circular>
+				</div>
+				<v-speed-dial
+					v-if="selectedFiles.length > 0"
+					bottom
+					fab
+					right
+					absolute
+					v-model="fab"
+				>
+					<template v-slot:activator>
+						<v-btn
+							color="blue darken-2"
+							dark
+							fab
+							hover
+							v-model="fab"
 						>
-							<prism-editor
-								v-if="!notSupported"
-								class="custom-font"
-								lineNumbers
-								v-model="fileContent"
-								:highlight="highlighter"
-							></prism-editor>
-						</v-card-text>
-						<v-card-actions v-if="!notSupported">
+							<v-icon v-if="fab">close</v-icon>
+							<v-icon v-else>settings</v-icon>
+						</v-btn>
+					</template>
+					<v-btn fab dark small color="green" @click="downloadZip">
+						<v-icon>file_download</v-icon>
+					</v-btn>
+					<v-btn fab dark small color="red" @click="deleteSelected">
+						<v-icon>delete</v-icon>
+					</v-btn>
+				</v-speed-dial>
+			</v-col>
+
+			<v-divider class="mx-0" vertical></v-divider>
+
+			<v-col class="text-center overflow-y-auto d-flex justify-center">
+				<div
+					v-if="!selected || !selected.ext"
+					class="
+						title
+						grey--text
+						text--lighten-1
+						font-weight-light
+						align-self-center
+					"
+				>
+					<v-icon color="grey lighten-4" x-large>
+						text_snippet
+					</v-icon>
+					<br />
+					Please select a file
+				</div>
+				<div
+					v-else-if="loadingFile"
+					class="
+						title
+						grey--text
+						text--lighten-1
+						font-weight-light
+						align-self-center
+					"
+				>
+					<v-progress-circular
+						indeterminate
+						color="primary"
+					></v-progress-circular>
+				</div>
+				<div
+					class="fill-remaining-space flex-grow-1"
+					v-else
+					:key="selected.path"
+				>
+					<div class="file-content pa-4 pb-8">
+						<prism-editor
+							v-if="!notSupported"
+							class="custom-font"
+							lineNumbers
+							v-model="fileContent"
+							:highlight="highlighter"
+						></prism-editor>
+					</div>
+					<div class="sticky-bottom pa-0" v-if="!notSupported">
+						<v-toolbar>
 							<v-spacer></v-spacer>
 							<v-btn
 								color="purple darken-1"
@@ -126,37 +158,16 @@
 								DOWNLOAD
 								<v-icon right dark>file_download</v-icon>
 							</v-btn>
-						</v-card-actions>
-					</v-card>
-				</v-col>
-			</v-row>
-		</v-card>
-		<v-speed-dial
-			v-if="selectedFiles.length > 0"
-			bottom
-			fab
-			right
-			fixed
-			v-model="fab"
-		>
-			<template v-slot:activator>
-				<v-btn color="blue darken-2" dark fab hover v-model="fab">
-					<v-icon v-if="fab">close</v-icon>
-					<v-icon v-else>settings</v-icon>
-				</v-btn>
-			</template>
-			<v-btn fab dark small color="green" @click="downloadZip">
-				<v-icon>file_download</v-icon>
-			</v-btn>
-			<v-btn fab dark small color="red" @click="deleteSelected">
-				<v-icon>delete</v-icon>
-			</v-btn>
-		</v-speed-dial>
+						</v-toolbar>
+					</div>
+				</div>
+			</v-col>
+		</v-row>
 	</v-container>
 </template>
-<style>
+<style scoped>
 /* optional class for removing the outline */
-.prism-editor-wrapper .prism-editor__textarea:focus {
+.prism-editor-wrapper >>> .prism-editor__textarea:focus {
 	outline: none !important;
 }
 
@@ -164,19 +175,34 @@
 	font-family: 'Fira Code', monospace;
 }
 
-.scroll {
-	overflow-y: scroll;
-	height: 100%;
-}
-
-.no-scroll {
-	overflow: hidden;
-}
-
 .full-height {
 	height: 100%;
 }
+
+.file-content {
+	font-size: 0.875rem;
+	width: 100%;
+	min-height: calc(100% - 64px);
+}
+
+.fill-remaining-space {
+	height: calc(100vh - 64px);
+	max-width: 100%;
+}
+
+.sticky-top {
+	position: sticky;
+	align-self: flex-start;
+	top: 64px; /** Top app bar height */
+}
+
+.sticky-bottom {
+	position: sticky;
+	align-self: flex-start;
+	bottom: 0;
+}
 </style>
+
 <script>
 import ConfigApis from '@/apis/ConfigApis'
 
