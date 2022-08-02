935a18d977d63087f4dcc6c6fd5a9d4e815e1e41
Daniel Lando <daniel.sorridi@gmail.com>
2022-08-02 18:13:01 +0200

    fix: icons not diaplaying correctly
    Fixes #2564

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -71,28 +71,15 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
-        }
+        type: 'asset/resource',
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('media/[name].[hash:7].[ext]')
-        }
+        type: 'asset/resource',
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]'),
-          publicPath: '../..'
-        }
+        type: 'asset/resource',
       },
       {
         test: /\.s(c|a)ss$/,
