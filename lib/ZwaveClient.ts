098432ea6e76270d80832496a903f29e69555fc5
Daniel Lando <daniel.sorridi@gmail.com>
2022-11-02 08:27:30 +0100

    feat(ui): allow to fetch OTA pre-releases (#2773)
    * feat(ui): allow to fetch OTA pre-releases
    
    Fixes #2768
    
    * style: align buttons

--- lib/ZwaveClient.ts
+++ lib/ZwaveClient.ts
@@ -72,6 +72,7 @@ import {
 	ZWaveNodeFirmwareUpdateFinishedCallback,
 	FirmwareUpdateResult,
 	FirmwareUpdateCapabilities,
+	GetFirmwareUpdatesOptions,
 } from 'zwave-js'
 import { getEnumMemberName, parseQRCodeString } from 'zwave-js/Utils'
 import { nvmBackupsDir, storeDir, logsDir } from '../config/app'
@@ -1909,11 +1910,15 @@ class ZwaveClient extends TypedEventEmitter<ZwaveClientEventCallbacks> {
 		}
 	}
 
-	async getAvailableFirmwareUpdates(nodeId: number) {
+	async getAvailableFirmwareUpdates(
+		nodeId: number,
+		options?: GetFirmwareUpdatesOptions
+	) {
 		if (this.driverReady) {
 			const result =
 				await this._driver.controller.getAvailableFirmwareUpdates(
-					nodeId
+					nodeId,
+					options
 				)
 
 			// return [
