3549d9c49ffbb2cc3e93367963ca5c7ce2a8bcce
Daniel Lando <daniel.sorridi@gmail.com>
2023-02-28 14:51:29 +0100

    fix: lint issues

--- src/lib/instanceManager.js
+++ src/lib/instanceManager.js
@@ -1,24 +1,26 @@
 const instances = {
-  APP: 'app',
-  ROUTER: 'router',
-  STORE: 'store',
-};
-
-class InstanceManager {
-  _instances = new Map();
-
-  register(name, instance) {
-    this._instances.set(name, instance);
-  }
-
-  unRegister(name) {
-    this._instances.delete(name);
-  }
-
-  getInstance(name) {
-    return this._instances.get(name);
-  }
+	APP: 'app',
+	ROUTER: 'router',
+	STORE: 'store',
 }
 
-const manager = new InstanceManager();
-export { manager, instances };
+class InstanceManager {
+	constructor() {
+		this._instances = new Map()
+	}
+
+	register(name, instance) {
+		this._instances.set(name, instance)
+	}
+
+	unRegister(name) {
+		this._instances.delete(name)
+	}
+
+	getInstance(name) {
+		return this._instances.get(name)
+	}
+}
+
+const manager = new InstanceManager()
+export { manager, instances }
