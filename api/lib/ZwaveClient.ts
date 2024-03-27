9693ba57c1e3c00aa53dabc4e585c5b0788725c3
Daniel Lando <daniel.sorridi@gmail.com>
2024-03-27 09:36:25 +0100

    fix: set driver ready to false immediatly on driver error
    Ref #3647

--- api/lib/ZwaveClient.ts
+++ api/lib/ZwaveClient.ts
@@ -726,8 +726,10 @@ class ZwaveClient extends TypedEventEmitter<ZwaveClientEventCallbacks> {
 	}
 
 	public set driverReady(ready) {
-		this._driverReady = ready
-		this.emit('driverStatus', ready)
+		if (this._driverReady !== ready) {
+			this._driverReady = ready
+			this.emit('driverStatus', ready)
+		}
 	}
 
 	public get cntStatus() {
@@ -4380,6 +4382,7 @@ class ZwaveClient extends TypedEventEmitter<ZwaveClientEventCallbacks> {
 
 		if (!skipRestart && error.code === ZWaveErrorCodes.Driver_Failed) {
 			// this cannot be recovered by zwave-js, requires a manual restart
+			this.driverReady = false
 			this.backoffRestart()
 		}
 	}
