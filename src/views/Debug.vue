4c76e61cabb8930d248224f81cd8ab7e0a748638
Daniel Lando <daniel.sorridi@gmail.com>
2024-09-23 09:46:38 +0200

    fix(ui): increese debug logs window height
    Fixes #3908

--- src/views/Debug.vue
+++ src/views/Debug.vue
@@ -141,7 +141,7 @@ export default {
 			this.showSnackbar('Debug ' + (v ? 'activated' : 'disabled'))
 		},
 		newWindow() {
-			openInWindow('DEBUG')
+			openInWindow('DEBUG', 1000)
 		},
 	},
 	mounted() {
