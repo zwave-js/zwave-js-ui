8dbea79b2465010e8af5c9ce6e04c55c634a6320
robertsLando <daniel.sorridi@gmail.com>
2025-06-27 08:47:51 +0200

    fix: comment out custom colors for now

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -15,14 +15,14 @@ const opts = {
 	theme: {
 		options: { customProperties: true }, // enable sass/scss variables
 		themes: {
-			dark: {
-				purple: '#BA68C8',
-				primary: '#42A5F5',
-				warning: '#FFB74D',
-				success: '#81C784',
-				error: '#EF5350',
-				info: '#42A5F5',
-			},
+			// dark: {
+			// 	purple: '#BA68C8',
+			// 	primary: '#42A5F5',
+			// 	warning: '#FFB74D',
+			// 	success: '#81C784',
+			// 	error: '#EF5350',
+			// 	info: '#42A5F5',
+			// },
 		},
 	},
 }
