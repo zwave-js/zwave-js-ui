6bc3f10329a3b2229444bcd2870e49f849f9e642
robertsLando <daniel.sorridi@gmail.com>
2025-10-07 14:47:04 +0200

    fix: fake stick

--- esbuild-import-meta-url-shim.js
+++
@@ -1,5 +0,0 @@
-export const __import_meta_url =
-	typeof document === 'undefined'
-		? new (require('url'.replace('', '')).URL)('file:' + __filename).href
-		: (document.currentScript && document.currentScript.src) ||
-			new URL('main.js', document.baseURI).href
