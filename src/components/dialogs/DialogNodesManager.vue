da0f128d4bad8d74106136b0596670f46039177a
AlCalzone <info@zwave-js.io>
2026-02-24 08:41:30 +0100

    fix: check `result` exists before accessing `lowSecurityReason` (#4560)

--- src/components/dialogs/DialogNodesManager.vue
+++ src/components/dialogs/DialogNodesManager.vue
@@ -1412,9 +1412,12 @@ export default {
 				doneStep.text = `Node ${
 					this.nodeFound.id
 				} added with security ${this.nodeFound.security || 'None'}`
-				doneStep.error = result.lowSecurityReason
-					? this.getSecurityBootstrapError(result.lowSecurityReason)
-					: false
+				doneStep.error =
+					result && result.lowSecurityReason
+						? this.getSecurityBootstrapError(
+								result.lowSecurityReason,
+							)
+						: false
 				doneStep.success = !(result && result.lowSecurity)
 				this.pushStep(doneStep)
 			}
