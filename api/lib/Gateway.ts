fb73508efe459fa124ad8c9b4c1038a9762196d8
AlCalzone <dominic.griesel@openhomefoundation.org>
2026-06-11 12:27:32 +0200

    fix(gateway): apply numeric `postOperation` scaling directly (#4662)

--- api/lib/Gateway.ts
+++ api/lib/Gateway.ts
@@ -457,7 +457,7 @@ export default class Gateway {
 			}
 
 			if (valueConf) {
-				if (this._isValidOperation(valueConf.postOperation)) {
+				if (utils.isValidOperation(valueConf.postOperation)) {
 					let op = valueConf.postOperation
 
 					// revert operation to write
@@ -466,7 +466,7 @@ export default class Gateway {
 					else if (op.includes('+')) op = op.replace(/\+/, '-')
 					else if (op.includes('-')) op = op.replace(/-/, '+')
 
-					payload = eval(`${payload}${op}`)
+					payload = utils.applyOperation(payload, op)
 				}
 
 				if (valueConf.parseReceive) {
@@ -1973,8 +1973,11 @@ export default class Gateway {
 		let tmpVal = valueId.value
 
 		if (valueConf) {
-			if (this._isValidOperation(valueConf.postOperation)) {
-				tmpVal = eval(valueId.value + valueConf.postOperation)
+			if (utils.isValidOperation(valueConf.postOperation)) {
+				tmpVal = utils.applyOperation(
+					valueId.value,
+					valueConf.postOperation,
+				)
 			}
 
 			if (valueConf.parseSend) {
@@ -2426,14 +2429,6 @@ export default class Gateway {
 	}
 
 	/**
-	 * Checks if an operation is valid, it must exist and must contains
-	 * only numbers and operators
-	 */
-	private _isValidOperation(op: string): boolean {
-		return op && !/[^0-9.()\-+*/,]/g.test(op)
-	}
-
-	/**
 	 * Evaluate the return value of a custom parse Function
 	 *
 	 */
@@ -2728,7 +2723,7 @@ export default class Gateway {
 			switchValue = `37-${endpoint}-currentValue`
 		}
 
-		/* 
+		/*
 			Find the control switch of the device Brightness or Binary
 			If multilevel is not there use binary
 			Some devices use also endpoint + 1 as on/off/brightness... try to guess that too!
