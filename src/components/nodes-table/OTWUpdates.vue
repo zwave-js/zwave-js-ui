c5bcde0b17a6cda85f62bf657dd8dd6d7215f90b
Kris Linquist <1343376+klinquist@users.noreply.github.com>
2026-09-19 17:09:41 -0700

    fix(ui): improve firmware warning grammar

--- src/components/nodes-table/OTWUpdates.vue
+++ src/components/nodes-table/OTWUpdates.vue
@@ -47,7 +47,7 @@ export default {
 					`Firmware Upgrade`,
 					`<p>Are you sure you want to upgrade your controller to <b>v${update.version}</b>?</p>
 
-                    <p><strong>We don't take any responsibility if devices upgraded using Z-Wave JS don't work after an update. Always double-check that the correct update is about to be installed</strong></p>
+                    <p><strong>We are not responsible if a device stops working after being upgraded using Z-Wave JS. Always double-check that you are about to install the correct update.</strong></p>
 
                     <p>This will download the desired firmware update from the <a href="https://github.com/zwave-js/firmware-updates/">Z-Wave JS firmware update service</a> and start the upgrade process.</p>
 
